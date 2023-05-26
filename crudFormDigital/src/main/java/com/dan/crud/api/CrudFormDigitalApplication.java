2fd2dc400275d847f1ee79b83140bee85dd99c8c
Danilo Azevedo Santos <desenvolvedor12@carrera.com.br>
2023-05-26 18:21:35 -0300

    1o commit

---
+++ crudFormDigital/src/main/java/com/dan/crud/api/CrudFormDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.dan.crud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFormDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFormDigitalApplication.class, args);
+	}
+
+}
---
+++ crudFormDigital/src/test/java/com/dan/crud/api/CrudFormDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.crud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFormDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
